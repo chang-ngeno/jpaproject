516669b62090d1677d040cce53f63100fa4c4590
Daniel Chang'masa <danchangmasa@gmail.com>
2018-07-26 06:17:44 +0300

    springboot empty
    springboot empty project for porting the application.

---
+++ appdemo/src/main/java/tk/macoz/blog/appdemo/AppdemoApplication.java
@@ -0,0 +1,12 @@
+package tk.macoz.blog.appdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppdemoApplication.class, args);
+	}
+}
